6cdc1c0a5a2625d0b098e8ff94e2c314088a0977
theMungai <mungaithesimon@gmail.com>
2025-04-15 04:43:44 +0300

    Event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function handleFocus(){
+    console.log("Good!");
+    
+}
+
+function handleBlur(){
+    console.log('Hey! Eyes on me!');
+    
+}
+
+function EyesOnMe () {
+  return (
+    <div>
+      <button onBlur={handleBlur} onFocus={handleFocus}>
+        Eyes on me
+      </button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function handleChange(){
+    console.log('Entering password...');
+    
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password"
+                onChange={handleChange} 
+            />
+        </div>
     )
 }
 
